91bc081a888dbbfe748b7e1f5b40a2bcda2dea27
Nikita <sotheres@gmail.com>
2021-07-08 14:14:41 +0300

    v0.1

--- src/SAP.java
+++ src/SAP.java
@@ -1,24 +1,58 @@
+import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
 import edu.princeton.cs.algs4.Digraph;
 
 public class SAP {
+    Digraph digraph;
+
     public SAP(Digraph G) {
+        if (G == null) {
+            throw new IllegalArgumentException("Constructor argument is null");
+        }
+
+        digraph = new Digraph(G);
 
     }
 
     public int length(int v, int w) {
+        validateVertex(v, w);
 
+        BreadthFirstDirectedPaths paths = new BreadthFirstDirectedPaths(digraph, v);
+        if (paths.hasPathTo(w)) {
+            return paths.distTo(w);
+        } else {
+            return -1;
+        }
     }
 
     public int ancestor(int v, int w) {
+        validateVertex(v, w);
+
 
     }
 
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
+        validate(v, w);
+
 
     }
 
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+        validate(v, w);
 
+
+    }
+
+    private void validate(Iterable<Integer> a, Iterable<Integer> b) {
+        if (a == null || b == null) {
+            throw new IllegalArgumentException("Argument is null");
+        }
+    }
+
+    private void validateVertex(int a, int b) {
+        int v = digraph.V();
+        if (a < 0 || a >= v || b < 0 || b >= v) {
+            throw new IllegalArgumentException("Illegal vertex value");
+        }
     }
 
     public static void main(String[] args) {
--- src/WordNet.java
+++ src/WordNet.java
@@ -1,6 +1,39 @@
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+import java.util.HashSet;
+
 public class WordNet {
+    Digraph wordnet;
+    HashSet<String> wordSet;
+
     public WordNet(String synsets, String hypernyms) {
         validate(synsets, hypernyms);
+
+        int size = 0;
+        In in = new In(synsets);
+        String[] line;
+        String[] synet;
+
+        while (!in.isEmpty()) {
+            line = in.readLine().split(",");
+            synet = line[1].split(" ");
+            for (String s : synet) {
+                wordSet.add(s);
+            }
+            size++;
+        }
+
+        wordnet = new Digraph(size);
+        in = new In(hypernyms);
+        int id;
+
+        while(!in.isEmpty()) {
+             line = in.readLine().split(",");
+             id = Integer.parseInt(line[0]);
+             for (int i = 1; i < line.length; i++) {
+                 wordnet.addEdge(id, Integer.parseInt(line[i]));
+             }
+        }
     }
 
     public Iterable<String> nouns() {
@@ -9,17 +42,25 @@ public class WordNet {
 
     public boolean isNoun(String word) {
         validate(word);
+
+        return wordSet.contains(word);
     }
 
     public int distance(String nounA, String nounB) {
         validate(nounA, nounB);
+        if (!isNoun(nounA) || !isNoun(nounB)) {
+            throw new IllegalArgumentException("Not a WordNet noun");
+        }
+
+
     }
 
     public String sap(String nounA, String nounB) {
         validate(nounA, nounB);
-    }
+        if (!isNoun(nounA) || !isNoun(nounB)) {
+            throw new IllegalArgumentException("Not a WordNet noun");
+        }
 
-    public static void main(String[] args) {
 
     }
 
@@ -29,5 +70,9 @@ public class WordNet {
                 throw new IllegalArgumentException("Argument is null");
             }
         }
+    }
+
+    public static void main(String[] args) {
+
     }
 }
